e4d5e5117ff5bef2134b830b97132670c7ae8acc
magnuswretblad <mwdev81@gmail.com>
2023-05-08 12:03:25 +0200

    yolo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { FaAdn } from "react-icons/fa";
+import { BsSearch, BsCart3 } from "react-icons/bs";
 
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div>
+      <FaAdn />
+      <h1>MensWear</h1>
+      </div>
+      <div>
+      <NavLink to="/">HOME</NavLink>
+      <NavLink to="/product">PRODUCT</NavLink>
+      <NavLink to="/contact">CONTACT</NavLink>
+      <BsSearch />
+      <NavLink to="login">login</NavLink>
+      <BsCart3 />
+      
+
+
+
+
+      
+      </div>
+
+
+    </div>
   )
 }
 
